1c3aaefd19553ec249189ae3e796717facdd5f68
Georgy Parhomenko <parhomenkogeorge@gmail.com>
2021-05-03 18:12:45 +0300

    implement packing

--- bins/packings.py
+++ bins/packings.py
@@ -31,3 +31,30 @@ def first_fit(bin_weight: int, element_weights: List[int]) -> int:
             bins.append([bin_weight - weight, [weight]])
             amount_of_required_bins += 1
     return amount_of_required_bins
+
+
+def best_fit(bin_weight: int, element_weights: List[int]) -> int:
+    amount_of_required_bins = 1
+    bins: List[List[int, List[int]]] = [[bin_weight, []]]
+    starting_min = bin_weight + 1
+    for weight in element_weights:
+        bin_index = 0
+        current_min = starting_min
+        for index, (space, _) in enumerate(bins):
+            if space <= current_min and space >= weight:
+                bin_index = index
+                current_min = space
+
+        if current_min == starting_min:
+            bins.append([bin_weight, [weight]])
+            amount_of_required_bins += 1
+        else:
+            bins[bin_index][1].append(weight)
+            bins[bin_index][0] -= weight
+    return amount_of_required_bins
+
+
+def first_fit_decreasing(bin_weight: int, element_weights: List[int]) -> int:
+    reversed_weights = element_weights[:]
+    reversed(reversed_weights)
+    return first_fit(bin_weight, reversed_weights)
--- bins/test_packing.py
+++ bins/test_packing.py
@@ -3,17 +3,20 @@ from math import ceil
 
 import pytest
 
-from packings import next_fit, first_fit
+from packings import best_fit, first_fit, next_fit, first_fit_decreasing
 
 
 @pytest.mark.parametrize("algo", [
     next_fit,
     first_fit,
+    best_fit,
+    first_fit_decreasing,
 ])
 def test_nf(algo):
     weights = [2, 5, 4, 7, 1, 3, 8]
     bin_weight = 10
-    assert algo(bin_weight, weights) >= min_amount_bins(bin_weight, weights)
+    amount_of_bins = algo(bin_weight, weights)
+    assert amount_of_bins >= min_amount_bins(bin_weight, weights)
 
 
 def min_amount_bins(bin_weight: int, weights: List[int]) -> int:
