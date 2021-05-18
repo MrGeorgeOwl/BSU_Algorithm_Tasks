bbdcf9cca2901d0d70761a8602e6b34e1f02b409
Georgy Parhomenko <g.parhomenko@itransition.com>
2021-05-18 23:14:55 +0300

    Fix packing algos

--- bins/packings.py
+++ bins/packings.py
@@ -1,61 +1,62 @@
-from random import randint
-from typing import List, Tuple
+import random
+import sys
 
-def next_fit(bin_weight: int, element_weights: List[int]) -> int:
-    current_bin_weight: int = bin_weight
-    amount_of_required_bins: int = 1
+
+def next_fit(bin_weight, element_weights):
+    bins = [bin_weight]
+    current_bin_index = 0
+    amount_of_required_bins = 1
     for weight in element_weights:
-        if current_bin_weight >= weight:
-            current_bin_weight -= weight
+        if bins[current_bin_index] > weight:
+            bins[current_bin_index] -= weight
         else:
-            current_bin_weight = bin_weight
+            current_bin_index += 1
+            bins.append(bin_weight - weight)
             amount_of_required_bins += 1
     return amount_of_required_bins
 
 
-def first_fit(bin_weight: int, element_weights: List[int]) -> int:
-    bins: List[int] = [bin_weight]
-    amount_of_required_bins = 1
+def first_fit(bin_weight, element_weights):
+    bins = [bin_weight]
     for weight in element_weights:
         for index, space in enumerate(bins):
             if space >= weight:
                 bins[index] -= weight
                 break
-            if index == len(bins) - 1:
+            elif index == len(bins) - 1:
                 bins.append(bin_weight - weight)
-                amount_of_required_bins += 1
                 break
-    return amount_of_required_bins
+    return len(bins)
 
 
-def best_fit(bin_weight: int, element_weights: List[int]) -> int:
+def best_fit(bin_weight, element_weights):
     amount_of_required_bins = 1
-    bins: List[int] = [bin_weight]
-    starting_min = bin_weight + 1
+    bins = [bin_weight]
+    starting_min = sys.maxsize
     for weight in element_weights:
         bin_index = 0
-        current_min = starting_min
+        min_difference = starting_min
         for index, space in enumerate(bins):
-            if space <= current_min and space >= weight:
+            difference = space - weight
+            if difference >= 0 and difference < min_difference:
                 bin_index = index
-                current_min = space
+                min_difference = difference
 
-        if current_min == starting_min:
-            bins.append(bin_weight)
+        if bins[bin_index] - min_difference < 0:
+            bins.append(bin_weight - weight)
             amount_of_required_bins += 1
         else:
             bins[bin_index] -= weight
     return amount_of_required_bins
 
 
-def first_fit_decreasing(bin_weight: int, element_weights: List[int]) -> int:
-    reversed_weights = element_weights[:]
-    reversed(reversed_weights)
-    return first_fit(bin_weight, reversed_weights)
+def first_fit_decreasing(bin_weight, element_weights):
+    return first_fit(bin_weight, sorted(element_weights, reverse=True))
 
 
 if __name__ == "__main__":
-    weights = [randint(1, 10) for _ in range(10_000)]
-    bin_weight = 10
+    random.seed(100)
+    weights = [random.random() for _ in range(10_000)]
+    bin_weight = 1
     for algo in (next_fit, first_fit, best_fit, first_fit_decreasing):
         print(f"Amount of required bins {algo(bin_weight, weights)} for algo: {algo.__name__}")
