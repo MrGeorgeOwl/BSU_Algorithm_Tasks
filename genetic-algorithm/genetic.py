ab608b6ed5ead7783cc4ca7985bbed3df3ab7544
Georgy Parhomenko <g.parhomenko@itransition.com>
2021-06-01 23:54:47 +0300

    Refactor code

--- genetic-algorithm/genetic.py
+++ genetic-algorithm/genetic.py
@@ -1,69 +1,143 @@
 import random
+from dataclasses import dataclass
+from typing import List, Tuple, Optional
+
 from numpy import mean
 
 BORDER = 100
 POPULATION_AMOUNT = 1000
-RESULT = 13
+RESULT = -50
 SELECTION_SIZE = round(0.6 * POPULATION_AMOUNT)
 RANDOM_SUBSET_SIZE = POPULATION_AMOUNT
 MUTATION_PROBABILITY = 10
 CHILD_TO_MUTATE = round(SELECTION_SIZE / 4)
 
 
-def f(u, w, x, y, z):
-    return w * y + z + u ** 2 * x ** 2 * y * z ** 2 + u * w * x ** 2 * y ** 2 * z ** 2 + w * x * y ** 2 * z ** 2
+@dataclass
+class Individual:
+    u: int
+    w: int
+    x: int
+    y: int
+    z: int
+
+    def __init__(
+            self,
+            u: Optional[int] = None,
+            w: Optional[int] = None,
+            x: Optional[int] = None,
+            y: Optional[int] = None,
+            z: Optional[int] = None,
+    ):
+        self.u = u or random.randint(-BORDER, BORDER)
+        self.w = w or random.randint(-BORDER, BORDER)
+        self.x = x or random.randint(-BORDER, BORDER)
+        self.y = y or random.randint(-BORDER, BORDER)
+        self.z = z or random.randint(-BORDER, BORDER)
+
+    def get_params(self):
+        return self.u, self.w, self.x, self.y, self.z
+
+    def mutate(self):
+        for attr in ("u", "w", "x", "y", "z"):
+            setattr(
+                self,
+                attr,
+                (random.randint(-50, 50) if random.random() * 100 > MUTATION_PROBABILITY
+                 else getattr(self, attr)),
+            )
 
 
-def target_assessment(args):
-    return abs(RESULT - f(*args))
+
+def multipoint_crossover(
+        first_parent: Individual,
+        second_parent: Individual,
+) -> Tuple[Individual, Individual]:
+    first_params = first_parent.get_params()
+    second_params = second_parent.get_params()
+    return (
+        Individual(*(first_params[:2] + second_params[2:4] + first_params[4:])),
+        Individual(*(second_params[:2] + first_params[2:4] + second_params[4:])),
+    )
 
 
-def tournament_selection(population):
+def onepoint_crossover(
+        first_parent: Individual,
+        second_parent: Individual,
+) -> Tuple[Individual, Individual]:
+    first_params = first_parent.get_params()
+    second_params = second_parent.get_params()
+    return (
+        Individual(*(first_params[:3] + second_params[3:])),
+        Individual(*(second_params[:3] + first_params[3:]))
+    )
+
+
+def f(individual: Individual):
+    u, w, x, y, z = individual.get_params()
+    return (
+            u ** 2 * w ** 2 * x ** 2 * y * z ** 2
+            + z
+            + u ** 2 * w * y * z
+            + u * w * y * z ** 2
+            + x * y
+    )
+
+
+def fitness(individual: Individual):
+    return abs(RESULT - f(individual))
+
+
+def tournament_selection(population: List[Individual]):
     random_subset = random.sample(population, RANDOM_SUBSET_SIZE)
-    return sorted(random_subset, key=target_assessment)[:SELECTION_SIZE]
+    return sorted(random_subset, key=fitness)[:SELECTION_SIZE]
 
 
-def count_probability(first_child, second_child):
-    first_elem_probability = target_assessment(second_child) / (target_assessment(first_child) + target_assessment(
-        second_child))
-    return [first_elem_probability, 1 - first_elem_probability]
+def make_crossover(
+        population: List[Individual],
+) -> List[Individual]:
+    amount_of_parents = round(len(population) / 2)
+    first_parents = [
+        population.pop(random.randint(0, len(population) - 1))
+        for _ in range(amount_of_parents)
+    ]
+    children_pairs_multi = [
+        multipoint_crossover(first_parent, second_parent)
+        for first_parent, second_parent in zip(first_parents, population)
+    ]
+    children_pairs_onepoint = [
+        onepoint_crossover(first_parent, second_parent)
+        for first_parent, second_parent in zip(first_parents, population)
+    ]
+    children_pairs_onepoint.extend(children_pairs_multi)
+    return [
+        child for pair in children_pairs_onepoint for child in pair
+    ]
 
 
-def proportional_crossover(first_child, second_child):
-    return tuple([random.choices(elem, count_probability(first_child, second_child))[0] for elem in
-                  zip(first_child, second_child)])
+def mutation(population: List[Individual]):
+    for child in list(reversed(list(sorted(population, key=fitness))))[:CHILD_TO_MUTATE]:
+        child.mutate()
 
 
-def make_crossover(population):
-    return [proportional_crossover(first_child, population[index + 1]) for index, first_child in
-            list(enumerate(population[:-1]))]
-
-
-def mutation(population):
-    for index, child in list(reversed(list(enumerate(sorted(population, key=target_assessment)))))[:CHILD_TO_MUTATE]:
-        population[index] = tuple(
-            [num if MUTATION_PROBABILITY <= random.randint(1, 100) else random.randint(-50, 50) for num in child])
-    return population
-
-
-def substitution(population, new_children):
-    population.sort(key=target_assessment, reverse=True)
-    new_children.sort(key=target_assessment, reverse=True)
-    for index, parent, child in zip(range(len(population) - 1, -1, -1), population, new_children):
-        population[index] = random.choices((parent, child), count_probability(parent, child))[0]
-    return population
+def substitution(new_children: List[Individual]) -> List[Individual]:
+    return list(sorted(new_children, key=fitness))[:POPULATION_AMOUNT]
 
 
 def genetic():
-    population = [tuple(random.sample(range(-BORDER, BORDER), 5)) for _ in range(POPULATION_AMOUNT)]
-    target_assessments = [10, ]
-    while target_assessments[0] != 0:
-        population = substitution(population, mutation(make_crossover(tournament_selection(population))))
-        target_assessments = sorted([target_assessment(parent) for parent in population])
+    population = [Individual() for _ in range(POPULATION_AMOUNT)]
+    while True:
+        children = make_crossover(
+            tournament_selection(population),
+        )
+        mutation(children)
+        population = substitution(children)
+        target_assessments = sorted([fitness(parent) for parent in population])
         print(f'min - {target_assessments[0]}; avg - {mean(target_assessments)}')
-    print(sorted(population, key=target_assessment)[0])
+        if target_assessments[0] == 0:
+            break
+    print(sorted(population, key=fitness)[0].get_params())
 
 
 if __name__ == '__main__':
-    random.seed(100)
     genetic()
